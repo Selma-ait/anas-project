b8fb2ac3e13f5bb3d2df7cc5e673c7b03b1d25e3
Anas AIT OUKHADOU <fryekh@MacBook-Air-de-Anas.local>
2025-03-03 18:15:39 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Welcome Back Anas! </h1>
+    <h4>Here's your biography </h4>
+
+      <div>
+        <a href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRexOe3x-fWKY1-_eAlyMZ_bArlfGkaBZ3Rs5go9JVCfG5NVc338H-BLDCXsuGR5J8N_DbG1DJKoHWFlWj6vpI9mg" target="_blank">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRexOe3x-fWKY1-_eAlyMZ_bArlfGkaBZ3Rs5go9JVCfG5NVc338H-BLDCXsuGR5J8N_DbG1DJKoHWFlWj6vpI9mg" className="logo" alt="Vite logo" />
+        </a>
+      
+      </div>
+      
+      <h1>Anas bernieri</h1>
+
+      <div className="card">
+       <p>The very rare Madagascar or Bernier’s teal (Anas bernieri Hartlaub 1860) is one of the rarest ducks and, till some time ago, less known. Enedemic to Madagascar, it belongs to the order of the Anseriforms (Anseriformes), to the family of the Anatids (Anatidae), to the genus Anas and to the species Anas bernieri.
+       The name of the genus “Anas” comes from the Latin terms “anas, anatis” simply meaning “duck”; the species “bernieri” is honoured to Chevalier J.A. Bernier, surgeon-naturalist who carried collector activity in Madagascar, between the 1831 and the 1834.</p>
+      </div>
+      <a href='https://www.monaconatureencyclopedia.com/anas-bernieri/?lang=en' className="read-the-docs">
+        Click here to learn more
+      </a>
+    </>
+  )
+}
+
+export default App
